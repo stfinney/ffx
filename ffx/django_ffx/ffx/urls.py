3a38f6c153f1135cc85d0dac0a36e51a09f661aa
Joseph Wu <josephwu.wyz@hotmail.com>
2015-12-05 15:40:07 -0800

    add event detail and links

--- ffx/django_ffx/ffx/urls.py
+++ ffx/django_ffx/ffx/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^events/', views.index, name='index'),
+    url(r'^events/$', views.index, name='event_index'),
+    url(r'^events/(?P<event_id>[0-9]+)/$', views.show, name='event_detail'),
+
 ]
